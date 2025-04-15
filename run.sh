25400aab99240564ad556947f6fcd707a8047d23
Cesar Chavez <cesarchanalu@gmail.com>
2025-04-15 17:35:26 +0200

    Updates

--- run.sh
+++ run.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
+# Step 1: Clone the GitHub repository (uncomment if starting fresh)
+# git clone https://github.com/CesarChaMal/roop.git
+# cd roop
 
-# Step 1: Clone the GitHub repository
-#git clone https://github.com/CesarChaMal/roop.git
-#cd roop
-
-# Step 2: Install dependencies
+# Step 2: Create env and Install Python dependencies
+python -m venv .venv
+source .venv/scripts/activate
 pip install -r requirements.txt
 
-# Step 3: Download the model and move it to the 'models' directory
-#wget https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx -O inswapper_128.onnx
+# Step 3: Download the ONNX model and move it to the models directory
+# wget https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx -O inswapper_128.onnx
 curl -L https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx -o inswapper_128.onnx
 
 mkdir -p models
-mv inswapper_128.onnx ./models
+mv inswapper_128.onnx ./models/
 
-# Step 4: Run the Python script
-# Replace '/content/video.mp4' and '/content/image.jpeg' with your actual file paths
-python run.py --target /video.mp4 --source /image.png -o /swapped.mp4 --execution-provider cuda --frame-processor face_swapper face_enhancer
+# Step 4a: Run face swap for IMAGE
+python run.py \
+  --target /content/target_image.png \
+  --source /content/source_image.png \
+  -o /content/swapped_image.png \
+  --execution-provider cuda \
+  --frame-processor face_swapper face_enhancer
+
+# Step 4b: Run face swap for VIDEO
+python run.py \
+  --target /content/target_video.mp4 \
+  --source /content/source_image.jpeg \
+  -o /content/swapped_video.mp4 \
+  --execution-provider cuda \
+  --frame-processor face_swapper face_enhancer
